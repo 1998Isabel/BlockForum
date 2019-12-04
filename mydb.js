3930befd3e532a4fc2d5d28c1d0373d5af0fab7b
1998isabel <1998isabel@gmail.com>
2019-12-04 19:44:46 +0800

    fake-mydb

---
+++ mydb.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+        addr: "xxxx",
+        name: "test",
+    }
+]
+
+const categories = [
+    {
+        id: 1,
+        name: "All",
+    }
+]
+
+const posts = [
+    {
+        id: 1,
+        cat: 1,
+        title: "Post1",
+        content: "This is the first post!",
+    }
+]
+
+const db = {
+    users,
+    categories, 
+    posts,
+}
+
+export default db;
