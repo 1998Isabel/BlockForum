a7a58c50ef8f185612e1cd205f08b474f6dc706d
Kao Pei-Wei <tif135@gmail.com>
2019-12-05 15:14:30 +0800

    add post/delete

--- mydb.js
+++ mydb.js
@@ -9,6 +9,14 @@ const categories = [
   {
     id: 1,
     name: "All"
+  },
+  {
+    id: 2,
+    name: "Music"
+  },
+  {
+    id: 3,
+    name: "News"
   }
 ];
 
@@ -18,6 +26,18 @@ const posts = [
     category: 1,
     title: "Post1",
     content: "This is the first post!"
+  },
+  {
+    id: 2,
+    category: 2,
+    title: "Post2",
+    content: "This is the 2nd post!"
+  },
+  {
+    id: 3,
+    category: 1,
+    title: "Post3",
+    content: "This is the 3rd post!"
   }
 ];
 
--- server.js
+++ server.js
@@ -9,6 +9,23 @@ app.get("/posts", (req, res) => {
   res.json(db.posts);
 });
 
+app.post("/posts", (req, res) => {
+  const newPost = {
+    id: req.body.id,
+    category: req.body.category,
+    title: req.body.title,
+    content: req.body.content
+  };
+
+  db.posts.push(newPost);
+  res.json(newPost);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  db.posts = db.posts.filter(post => post.id !== parseInt(req.params.id));
+  res.json(db.posts);
+});
+
 const port = process.env.PORT || 7000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
