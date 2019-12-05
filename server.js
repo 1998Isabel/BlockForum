2d76fbe3b90af1ba2ce5448d28080417b7b20a48
Kao Pei-Wei <tif135@gmail.com>
2019-12-05 15:22:40 +0800

    update

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 app.use(express.json());
 
+// Users METHODS
+app.get("/user/:addr", (req, res) => {
+  const user = db.users.filter(user => user.addr === req.params.addr);
+  res.json(user);
+});
+
+// Posts METHODS
 app.get("/posts", (req, res) => {
   res.json(db.posts);
 });
@@ -24,6 +31,28 @@ app.post("/posts", (req, res) => {
 app.delete("/posts/:id", (req, res) => {
   db.posts = db.posts.filter(post => post.id !== parseInt(req.params.id));
   res.json(db.posts);
+});
+
+// Categories METHODS
+app.get("/categories", (req, res) => {
+  res.json(db.categories);
+});
+
+app.post("/categories", (req, res) => {
+  const newCategory = {
+    id: req.body.id,
+    name: req.body.name
+  };
+
+  db.categories.push(newCategory);
+  res.json(newCategory);
+});
+
+app.delete("/categories/:id", (req, res) => {
+  db.categories = db.categories.filter(
+    category => category.id !== parseInt(req.params.id)
+  );
+  res.json(db.categories);
 });
 
 const port = process.env.PORT || 7000;
