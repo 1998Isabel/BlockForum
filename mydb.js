d4540f42371c2fe08c145deaa09a9cb26fa06a29
1998isabel <1998isabel@gmail.com>
2019-12-09 17:29:28 +0800

    changedb

--- mydb.js
+++ mydb.js
@@ -19,22 +19,23 @@ const posts = [
     category: "Economics",
     title: "Post1",
     content: "This is the first post!",
-    date: Date.now()
+    date: Date.now(),
+    user: "0xE195c59BCF26fD36c82d1C720860127A5c1c4040",
   },
-  {
-    id: "2",
-    category: "Sports",
-    title: "Post2",
-    content: "This is the 2nd post!",
-    date: Date.now()
-  },
-  {
-    id: "3",
-    category: "News",
-    title: "Post3",
-    content: "This is the 3rd post!",
-    date: Date.now()
-  }
+  // {
+  //   id: "2",
+  //   category: "Sports",
+  //   title: "Post2",
+  //   content: "This is the 2nd post!",
+  //   date: Date.now()
+  // },
+  // {
+  //   id: "3",
+  //   category: "News",
+  //   title: "Post3",
+  //   content: "This is the 3rd post!",
+  //   date: Date.now()
+  // }
 ];
 
 const db = {
