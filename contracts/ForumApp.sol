2b4f4aafe7f118615ac6c38576ebc9aca30085ee
Henry Laih <henrylaih41@yahoo.com>
2019-12-31 14:48:29 +0800

    addPostHash

--- contracts/ForumApp.sol
+++ contracts/ForumApp.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract ForumApp {
 
     event OnPostAdded(uint postId);
+    event OnPostHashAdded(uint post_hashId);
     event OnCatAdded(uint catId);
     event OnUserAdded(uint userId);
 
@@ -19,17 +20,33 @@ contract ForumApp {
         string user;
         string image_hash;
         uint   date;
+    }
+
+    struct PostHash {
+        string post_id;
         string post_hash;
     }
 
     Post[] posts;
     User[] users;
+    PostHash[] postHashs;
     string[] categorys;
+
+    function addPostHash(string memory id, string memory post_hash) public {
+        PostHash memory postHash = PostHash({
+            post_id: id,
+            post_hash: post_hash
+        });
+
+        uint post_hashId = postHashs.push(postHash) - 1;
+        emit OnPostHashAdded(post_hashId);
+
+    }
 
     function addPost(string memory id,    string memory category,
                      string memory title, string memory content,
                      string memory user,  uint   date,
-                     string memory image_hash, string memory post_hash) public {
+                     string memory image_hash) public {
         Post memory post = Post({
             id: id, 
             category: category, 
@@ -37,18 +54,24 @@ contract ForumApp {
             content:  content,
             user:     user, 
             date:     date,
-            image_hash: image_hash,
-            post_hash: post_hash
+            image_hash: image_hash
         });
             
         uint postId = posts.push(post) - 1;
         emit OnPostAdded(postId);
     }
-
+    function getPostHashLength() public view returns(uint length){
+        return postHashs.length;
+    }
     function getPostLength() public view returns(uint length){
         return posts.length;
     }
     
+    function getPostHashes(uint idx) public view 
+    returns(string memory id, string memory post_hash){
+        PostHash memory postHash = postHashs[idx];
+        return (postHash.post_id, postHash.post_hash);
+    }
     function getPosts(uint idx) public view 
     returns(string memory id,    string memory category, 
             string memory title, string memory content,
@@ -61,13 +84,6 @@ contract ForumApp {
                post.user,  post.date,
                post.image_hash);
     } 
-
-    function getPostsHash(uint idx) public view 
-    returns(string memory post_hash){
-        Post memory post = posts[idx];
-        return post.post_hash;
-    } 
-
 
     function addCategory(string memory category) public {
         uint catId = categorys.push(category) - 1;
